3e631dd26c457faa3d08acb23591e4b4b6ab31f4
stovegit <stovebruh@gmail.com>
2017-06-23 09:43:00 -0400

    should be good

---
+++ script.js
@@ -0,0 +1,10 @@
+var hi = window.alert("hello")
+var food = prompt("What is your food total?")
+var tip = prompt("What would you like your desired tip percentage to be?")
+
+
+
+// calculator
+
+var thingy = document.getElementById("total")
+thingy.innerHTML = (food * tip * .01) + food * 1.0
